acc29b3b491e24b71e57e88206ab14e6416ef01b
Krista Singleton <kris.singlet231@gmail.com>
2020-11-18 19:06:50 -0700

    Made several small configuration changes

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -3,8 +3,18 @@ import React from 'react';
 class App extends React.Component {
   constructor() {
     super();
+    this.state = {
+      page: 'home'
+    }
+    this.changePage = this.changePage.bind(this);
   }
+
+  changePage(newPage) {
+    this.setState({ page: newPage });
+  }
+
   render () {
+    let level;
     return (
       <div>
         Ollo?
