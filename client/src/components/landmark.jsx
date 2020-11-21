e62b831dab1284ea820c2d5074365a5c5d543f93
Krista Singleton <kris.singlet231@gmail.com>
2020-11-20 22:21:07 -0700

    Added traveling and landmark components files

--- client/src/components/landmark.jsx
+++ client/src/components/landmark.jsx
@@ -1,1 +1,10 @@
 import React from 'react';
+
+const Landmark = () => {
+  let landmark;
+  return (
+    <div>This is the Landmark page</div>
+  );
+};
+
+export default Landmark;
--- client/src/components/traveling.jsx
+++ client/src/components/traveling.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
 const TravelingPage = () => {
+  let traveling;
   return (
     <div>This is the Traveling screen</div>
-  )
-}
+  );
+};
 
 export default TravelingPage;
