b2a7bad67ba9d2498d5b5fcb197090580febbd91
Krista Singleton <kris.singlet231@gmail.com>
2020-11-22 17:19:42 -0700

    Added rover to global state and passed StatusScreen component crew and rover states

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -15,9 +15,11 @@ class App extends React.Component {
     this.state = {
       page: 'main',
       crew: [],
+      rover: '',
     };
     this.changePage = this.changePage.bind(this);
     this.changeCrew = this.changeCrew.bind(this);
+    this.changeRover = this.changeRover.bind(this);
   }
 
   changePage(newPage) {
@@ -28,6 +30,11 @@ class App extends React.Component {
     e.preventDefault();
     this.setState({ crew: crewList });
     console.log(this.state.crew);
+  }
+
+  changeRover(e, roverType) {
+    e.preventDefault();
+    this.setState({ rover: roverType });
   }
 
   render() {
@@ -46,7 +53,7 @@ class App extends React.Component {
       );
     } else if (this.state.page === 'rover') {
       level = (
-        <ChooseRover changePage={this.changePage} />
+        <ChooseRover changePage={this.changePage} changeRover={this.changeRover} />
       );
     } else if (this.state.page === 'supplies') {
       level = (
@@ -56,7 +63,7 @@ class App extends React.Component {
       level = (
         <div>
           <Landmark changePage={this.changePage} />
-          <StatusScreen />
+          <StatusScreen rover={this.state.rover} crew={this.state.crew} />
         </div>
       );
     } else if (this.state.page === 'traveling') {
