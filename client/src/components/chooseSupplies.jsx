f502f8c387c7e38a4d82b55cd9ea47092f2feda1
Krista Singleton <kris.singlet231@gmail.com>
2020-11-23 16:09:45 -0700

    Created supply list for chooseSupplies component

--- client/src/components/chooseSupplies.jsx
+++ client/src/components/chooseSupplies.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const ChooseSupplies = ({ changePage }) => {
-  console.log('ChooseSupplies fired');
+  const supplies = [{ type: 'Oxygen Tank', weight: 10 }, { type: 'Freeze-Dried-Food-Pack', weight: 1 }, { type: 'Water-Pouch', weight: 2 }, { type: 'Regular Clothes Set', weight: 3 }, { type: 'Warm Clothes Set', weight: 6 }, { type: 'Regular Space Suit', weight: 5 }, { type: 'Dust Proof Space Suit', weight: 10 }, { type: 'AI Maintenance Toolkit', weight: 5 }, { type: 'Rover Tire Patch', weight: 2 }, { type: 'Rover Maintenance Toolkit', weight: 8 }];
   return (
     <div>
       <h3>This is the Supply Choosing Page</h3>
