21904996c2e7a115b92a62a7f8f69484852b70ae
Krista Singleton <kris.singlet231@gmail.com>
2020-11-23 23:31:09 -0700

    Scrapped working problem for chooseSupplies component for a different direction

--- client/src/components/chooseSupplies.jsx
+++ client/src/components/chooseSupplies.jsx
@@ -1,40 +1,44 @@
-import React, { useState } from 'react';
+import React from 'react';
 import data from '../../dist/data.json';
 import classes from '../css/styles.css';
 
 const ChooseSupplies = ({ changePage }) => {
-  const useInput = (initialValue) => {
+  console.log(data.supplyList);
+  return (
+    <div className={classes.supplyPage}>
+      <h3>This is the Supply Choosing Page</h3>
+      <button type="button">What should we take?</button>
+      <div>Ollo</div>
+      <button type="button" onClick={() => { changePage('landmark'); }}>Begin journey</button>
+    </div>
+  );
+};
+
+export default ChooseSupplies;
+
+/*  const useInput = (initialValue) => {
     const [value, setValue] = useState(initialValue);
     const handleChange = (e) => {
       setValue(e.target.value);
     };
     return [value, handleChange];
   };
-  const supplyList = data.supplyList.map((supply) => (
+        <form onSubmit={() => { changePage('landmark'); }}>
+          {supplyList}
+          <input type="submit" value="Begin Journey" />
+        </form>
+const [listedSupply, handleSupplyChange] = useInput('');
+  const handleSupplyInputChange = (e) => {
+    handleSupplyChange(e.target.value);
+    console.log(listedSupply);
+  };
+ const supplyList = data.supplyList.map((supply) => (
     <div className={classes.supplyList}>
       <label>
         {supply.type}
         :
       </label>
-      <input type="text" className={classes.supplyInput} weight={supply.weight} onChange={} />
+      <input type="text" className={classes.supplyInput} weight={supply.weight} value={0} onChange={handleSupplyInputChange} />
     </div>
   ));
-  const [oxygen, handleOxyChange] = useState('MiniCoup');
-  const findRover = (e) => {
-    handleRoverChange(e.target.value);
-  };
-  return (
-    <div>
-      <h3>This is the Supply Choosing Page</h3>
-      <button type="button">What should I take?</button>
-      <div>
-        <form onSubmit={() => { changePage('landmark'); }}>
-          {supplyList}
-          <input type="submit" value="Begin Journey" />
-        </form>
-      </div>
-    </div>
-  );
-};
-
-export default ChooseSupplies;
+*/
