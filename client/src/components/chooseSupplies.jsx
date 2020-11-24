e7f0554ce94dfe06edb1875d562916be50ce1714
Krista Singleton <kris.singlet231@gmail.com>
2020-11-23 23:16:27 -0700

    Changed 'chooseSupplies' back to a functional component for better code cleanliness

--- client/src/components/chooseSupplies.jsx
+++ client/src/components/chooseSupplies.jsx
@@ -1,42 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import data from '../../dist/data.json';
 import classes from '../css/styles.css';
 
-class ChooseSupplies extends React.Component {
-  constructor({ changePage }) {
-    super({ changePage });
-    this.state = {
-      oxy: {
-        amount: 0,
-        totalWeight: 0,
-      },
+const ChooseSupplies = ({ changePage }) => {
+  const useInput = (initialValue) => {
+    const [value, setValue] = useState(initialValue);
+    const handleChange = (e) => {
+      setValue(e.target.value);
     };
-  }
-
-  render() {
-    console.log(this.state.oxy);
-    const supplyList = data.supplyList.map((supply) => (
-      <div className={classes.supplyList}>
-        <label>
-          {supply.type}
-          :
-        </label>
-        <input type="text" className={classes.supplyInput} weight={supply.weight} />
-      </div>
-    ));
-    return (
+    return [value, handleChange];
+  };
+  const supplyList = data.supplyList.map((supply) => (
+    <div className={classes.supplyList}>
+      <label>
+        {supply.type}
+        :
+      </label>
+      <input type="text" className={classes.supplyInput} weight={supply.weight} onChange={} />
+    </div>
+  ));
+  const [oxygen, handleOxyChange] = useState('MiniCoup');
+  const findRover = (e) => {
+    handleRoverChange(e.target.value);
+  };
+  return (
+    <div>
+      <h3>This is the Supply Choosing Page</h3>
+      <button type="button">What should I take?</button>
       <div>
-        <h3>This is the Supply Choosing Page</h3>
-        <button type="button">What should I take?</button>
-        <div>
-          <form onSubmit={() => { this.changePage('landmark'); }}>
-            {supplyList}
-            <input type="submit" value="Begin Journey" />
-          </form>
-        </div>
+        <form onSubmit={() => { changePage('landmark'); }}>
+          {supplyList}
+          <input type="submit" value="Begin Journey" />
+        </form>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default ChooseSupplies;
