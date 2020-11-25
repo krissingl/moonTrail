39bdce616e090b3db6636060c37c9648960f8d81
Krista Singleton <kris.singlet231@gmail.com>
2020-11-24 21:24:24 -0700

    Added a max speed variable to chooseRover component

--- client/src/components/chooseRover.jsx
+++ client/src/components/chooseRover.jsx
@@ -10,14 +10,18 @@ const ChooseRover = ({ changePage, changeRover }) => {
     handleRoverChange(e.target.value);
   };
   let storageCapacity;
+  let maxSpeed;
   if (rover === 'MiniCoup') {
-    storageCapacity = 200;
+    storageCapacity = 400;
+    maxSpeed = 10;
   } else if (rover === 'MiddleMan') {
-    storageCapacity = 350;
-  } else if (rover === 'StationWagon') {
     storageCapacity = 500;
+    maxSpeed = 7;
+  } else if (rover === 'StationWagon') {
+    storageCapacity = 700;
+    maxSpeed = 5;
   }
-  console.log(storageCapacity);
+  console.log(`Current storage capacity: ${storageCapacity} and max speed: ${maxSpeed}`);
   return (
     <div>
       <h3>Choose the best Rover for your journey!</h3>
