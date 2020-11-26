423e7ddacfc622cb44119dd0665540aec68971ac
Krista Singleton <kris.singlet231@gmail.com>
2020-11-25 21:57:38 -0700

    Added rover images and they are fantastic

--- client/src/components/chooseRover.jsx
+++ client/src/components/chooseRover.jsx
@@ -1,4 +1,8 @@
 import React, { useState } from 'react';
+import classes from '../css/styles.css';
+import bug from '../../dist/extras/rovers/bug.gif';
+import jeep from '../../dist/extras/rovers/jeep.gif';
+import stationWag from '../../dist/extras/rovers/stationWagon.gif';
 
 const ChooseRover = ({ changePage, changeRover }) => {
   const rovers = ['MiniCoup', 'MiddleMan', 'StationWagon'];
@@ -11,15 +15,31 @@ const ChooseRover = ({ changePage, changeRover }) => {
   };
   let storageCapacity;
   let maxSpeed;
+  let roverImg;
   if (rover === 'MiniCoup') {
     storageCapacity = 400;
     maxSpeed = 10;
+    roverImg = (
+      <div>
+        <img className={classes.bugRoverImg} src={bug} alt="roverGif" />
+      </div>
+    );
   } else if (rover === 'MiddleMan') {
     storageCapacity = 500;
     maxSpeed = 7;
+    roverImg = (
+      <div>
+        <img className={classes.jeepRoverImg} src={jeep} alt="roverGif" />
+      </div>
+    );
   } else if (rover === 'StationWagon') {
     storageCapacity = 700;
     maxSpeed = 5;
+    roverImg = (
+      <div>
+        <img className={classes.sWRoverImg} src={stationWag} alt="roverGif" />
+      </div>
+    );
   }
   const getRoverObject = () => {
     const roverObj = {
@@ -29,7 +49,6 @@ const ChooseRover = ({ changePage, changeRover }) => {
     };
     return roverObj;
   };
-  console.log(`Current storage capacity: ${storageCapacity} and max speed: ${maxSpeed}`);
   return (
     <div>
       <h3>CHOOSE A ROVER TO USE FOR THIS MISSION</h3>
@@ -38,6 +57,12 @@ const ChooseRover = ({ changePage, changeRover }) => {
         <select onChange={findRover}>
           {roverList}
         </select>
+      </div>
+      <div>
+        <h5>{rover}</h5>
+        {roverImg}
+        <div>{`MAXIMUM STORAGE CAPACITY: ${storageCapacity}`}</div>
+        <div>{`MAXIMUM SPEED: ${maxSpeed}`}</div>
       </div>
       <button type="button" onClick={(e) => { const finalRover = getRoverObject(); changeRover(e, finalRover); changePage('supplies'); }}>Choose Supplies</button>
     </div>
