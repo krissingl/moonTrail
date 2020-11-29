5ae717ff18ea512aa1c33aeb7cb59af20b5318ce
Krista Singleton <kris.singlet231@gmail.com>
2020-11-28 22:54:22 -0700

    Added to the supplyAdvice component

--- client/src/components/supplyAdvice.jsx
+++ client/src/components/supplyAdvice.jsx
@@ -4,7 +4,25 @@ const SupplyAdvice = ({ changePage }) => {
   console.log('Supply Advice page fired');
   return (
     <div>
-      <h3>This is the supply Advice page</h3>
+      <h3>RECOMMENDED SUPLLIES FOR THIS MISSION</h3>
+      <div>OxygenTank…………………Should have at least 2 per Astro</div>
+      <br />
+      <div>
+        Freeze-Dried Food………….Food is deleted by crew consumption every 8 secs during travel
+      </div>
+      <br />
+      <div>OxygenTank…………………Should have at least 2 per Astro</div>
+      <br />
+      <div>OxygenTank…………………Should have at least 2 per Astro</div>
+      <br />
+      <div>OxygenTank…………………Should have at least 2 per Astro</div>
+      <br />
+      <div>OxygenTank…………………Should have at least 2 per Astro</div>
+      <br />
+      <div>OxygenTank…………………Should have at least 2 per Astro</div>
+      <br />
+      <div>OxygenTank…………………Should have at least 2 per Astro</div>
+      <br />
       <button type="button" onClick={() => { changePage('supplies'); }}>Back to Supplies</button>
     </div>
   );
