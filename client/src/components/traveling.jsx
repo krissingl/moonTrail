dac9f0682cb06ab70f9f34f794bd718d5d101748
Krista Singleton <kris.singlet231@gmail.com>
2020-11-29 21:55:23 -0700

    Added alert window to traveling component

--- client/src/components/traveling.jsx
+++ client/src/components/traveling.jsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AlertWindow from './popUpAlert.jsx';
 
 const TravelingPage = ({ changePage }) => {
-  console.log('TravelPage fired');
+  const [showAlert, toggleAlert] = useState(false);
+  const [alertMsg, changeAlertMsg] = useState('');
+  let alertPopUp;
+  if (showAlert) {
+    alertPopUp = (
+      <AlertWindow message={alertMsg} toggleAlert={toggleAlert} />
+    );
+  }
   return (
     <div>
       <h3>
         This is the Traveling screen
       </h3>
-      <AlertWindow />
+      {alertPopUp}
+      <button type="button" onClick={() => { changeAlertMsg('Someone died of dysentry.'); toggleAlert(true); }}>Someone Died</button>
       <button type="button" onClick={() => { changePage('landmark'); }}>Arrive at Landmark</button>
     </div>
   );
