81bfebd73512d134c6913d61b9a607876dc90978
Krista Singleton <kris.singlet231@gmail.com>
2020-11-29 23:57:03 -0700

    Added clearer verbiage to the gameover component

--- client/src/components/gameover.jsx
+++ client/src/components/gameover.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 
 const Gameover = () => {
-  let won;
-  let lost;
+  let finalScreen;
+  if (true) {
+    finalScreen = (
+      <div>
+        <h3>CONGRAGULATIONS YOU MADE IT HOME!</h3>
+        <h5>FINAL SCORE COUNT HERE</h5>
+      </div>
+    );
+  }
   return (
-    <div>This is the gameover screen</div>
+    <div>
+      {finalScreen}
+    </div>
   );
 };
 
