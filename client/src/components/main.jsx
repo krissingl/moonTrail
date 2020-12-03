550f8eca578afe7e2dc6769c696119c17c44bed9
Krista Singleton <kris.singlet231@gmail.com>
2020-12-03 00:00:37 -0700

    Capitalized 'start' button on main component

--- client/src/components/main.jsx
+++ client/src/components/main.jsx
@@ -4,7 +4,7 @@ import classes from '../css/styles.css';
 const Main = ({ changePage }) => (
   <div className={classes.noticePage}>
     <h1>WELCOME_TO_MOON_TRAIL</h1>
-    <button type="button" className={classes.startBtn} onClick={() => { changePage('intro'); }}>Start</button>
+    <button type="button" className={classes.startBtn} onClick={() => { changePage('intro'); }}>START</button>
   </div>
 );
 
