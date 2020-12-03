6186f6e462e29e222b83b987ffb462f9ebd280e6
Krista Singleton <kris.singlet231@gmail.com>
2020-12-02 23:55:24 -0700

    Change 'Main Menu' btn to all uppercase to go with theme

--- client/src/components/titleMenu.jsx
+++ client/src/components/titleMenu.jsx
@@ -3,7 +3,7 @@ import classes from '../css/styles.css';
 
 const TitleMenu = ({ changePage, reset }) => (
   <div className={classes.alwaysMenu}>
-    <button type="button" onClick={() => { reset(); changePage('main'); }}>Main Menu</button>
+    <button type="button" onClick={() => { reset(); changePage('main'); }}>MAIN MENU</button>
   </div>
 );
 
