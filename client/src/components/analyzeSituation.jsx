2da973f1ffd3958a8f642394826628d66c329bba
Krista Singleton <kris.singlet231@gmail.com>
2020-12-04 23:04:08 -0700

    Added AlertWindow component and alert function states to Analyzation component for check map functionality

--- client/src/components/analyzeSituation.jsx
+++ client/src/components/analyzeSituation.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import AlertWindow from './popUpAlert.jsx';
 import classes from '../css/styles.css';
 
 const Analyzation = ({ changePage, travelingStatus }) => {
@@ -8,11 +9,24 @@ const Analyzation = ({ changePage, travelingStatus }) => {
   } else {
     previous = 'landmark';
   }
+
+  const [showAlert, toggleAlert] = useState(false);
+  const [alertMsg, changeAlertMsg] = useState('');
+
+  let alertPopUp;
+  if (showAlert) {
+    alertPopUp = (
+      <AlertWindow message={alertMsg} toggleAlert={toggleAlert} />
+    );
+  }
   return (
     <div className={classes.noticePage}>
       <h3>
         ANALYZATION_OPTIONS...
       </h3>
+      <div>
+        {alertPopUp}
+      </div>
       <div className={classes.analyzeMenu}>
         <button type="button" className={classes.analyzeMenuBtn}>STOP_TO_REST</button>
         <button type="button" className={classes.analyzeMenuBtn}>ALTER_CREW_RATIONS</button>
