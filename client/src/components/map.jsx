5be95e729490fcd9f0c661f1d012a4b64264e578
Krista Singleton <kris.singlet231@gmail.com>
2020-12-04 23:53:09 -0700

    Renamed map image for clarity reasons

--- client/src/components/map.jsx
+++ client/src/components/map.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import mapImg from '../../dist/extras/moonPathImgs/download.png';
+import mapImg from '../../dist/extras/moonPathImgs/map.png';
 import classes from '../css/styles.css';
 
 const Map = () => {
