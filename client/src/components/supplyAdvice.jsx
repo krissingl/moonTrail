fbbf6ed5e337bd30d4c79c07a31fc7ecb124c3f4
Krista Singleton <kris.singlet231@gmail.com>
2020-12-07 23:06:45 -0700

    Added adivce data to the supplyAdvice compoennt

--- client/src/components/supplyAdvice.jsx
+++ client/src/components/supplyAdvice.jsx
@@ -3,44 +3,14 @@ import data from '../../dist/data.json';
 import classes from '../css/styles.css';
 
 const SupplyAdvice = ({ changePage }) => {
-  const advice = data.introVerbiage;
+  const advice = data.supplyAdvice;
   return (
     <div className={classes.noticePage}>
       <h3>RECOMMENDED_SUPLLIES_FOR_THIS_MISSION:</h3>
       <br />
-      <div>OXYGENTANK.....needed by crew constantly (reccomend: 2+ per crewmember)</div>
-      <br />
       <div>
-        FREEZE-DRIED FOOD.....needed by the crew constantly to maintain health
+        {advice}
       </div>
-      <br />
-      <div>WATER-POUCH.....needed by the crew constantly to maintain health</div>
-      <br />
-      <div>
-        REGULAR_CLOTHES_SET.....needed to maintain crew health (reccomend: 2+ sets per crewmember)
-      </div>
-      <br />
-      <div>
-        WARM_CLOTHES_SET.....warmer alternative to REGULAR_CLOTHES_SET
-      </div>
-      <br />
-      <div>
-        SPACE_SUIT.....each crewmember must have 1 at all times (reccomend: 1+ per crewmember)
-      </div>
-      <br />
-      <div>
-        DUST_PROOF_SPACE_SUIT.....safer alternative to the SPACE_SUIT (reccomend: 1+ per crewmember)
-      </div>
-      <br />
-      <div>
-        CACAL_MAINTENANCE_KIT.....needed to maintain
-        Communications_Artifical_Companion_and_Analytical_Locator(reccomend: 9+)
-      </div>
-      <br />
-      <div>ROVER_TIRE_PATCH.....must-have in case of rover flat tire (reccomend: 5+)</div>
-      <br />
-      <div>ROVER_MAINTENANCE_KIT.....must-have in case of rover breakdown (reccomend: 5+)</div>
-      <br />
       <br />
       <button type="button" onClick={() => { changePage('supplies'); }}>BACK TO SUPPLIES</button>
     </div>
