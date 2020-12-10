0c3c258777c1ee02ea5ded3d102aaa1831bba8cc
Krista Singleton <kris.singlet231@gmail.com>
2020-12-09 23:54:23 -0700

    Modified supplyObj function in supplyObj component to take a list of supplies as a parameter

--- client/src/components/supplyObj.jsx
+++ client/src/components/supplyObj.jsx
@@ -5,25 +5,25 @@ const SupplyObj = () => {
   const getFinalSupplyObj = (supplyList) => {
     const supplyObj = {
       oxygen:
-      { type: data.supplyList[0].type, weight: data.supplyList[0].weight, amount: oxyAmount },
+      { type: supplyList[0].type, weight: supplyList[0].weight, amount: oxyAmount },
       food:
-      { type: data.supplyList[1].type, weight: data.supplyList[1].weight, amount: foodAmount },
+      { type: supplyList[1].type, weight: supplyList[1].weight, amount: foodAmount },
       water:
-      { type: data.supplyList[2].type, weight: data.supplyList[2].weight, amount: waterAmount },
+      { type: supplyList[2].type, weight: supplyList[2].weight, amount: waterAmount },
       clothes:
-      { type: data.supplyList[3].type, weight: data.supplyList[3].weight, amount: clothesAmount },
+      { type: supplyList[3].type, weight: supplyList[3].weight, amount: clothesAmount },
       clothes2:
-      { type: data.supplyList[4].type, weight: data.supplyList[4].weight, amount: clothesAmount2 },
+      { type: supplyList[4].type, weight: supplyList[4].weight, amount: clothesAmount2 },
       spaceSuit:
-      { type: data.supplyList[5].type, weight: data.supplyList[5].weight, amount: suitAmount },
+      { type: supplyList[5].type, weight: supplyList[5].weight, amount: suitAmount },
       spaceSuit2:
-      { type: data.supplyList[6].type, weight: data.supplyList[6].weight, amount: suitAmount2 },
+      { type: supplyList[6].type, weight: supplyList[6].weight, amount: suitAmount2 },
       aiKit:
-      { type: data.supplyList[7].type, weight: data.supplyList[7].weight, amount: AImainAmount },
+      { type: supplyList[7].type, weight: supplyList[7].weight, amount: AImainAmount },
       tirePatch:
-      { type: data.supplyList[8].type, weight: data.supplyList[8].weight, amount: tirePatchAmount },
+      { type: supplyList[8].type, weight: supplyList[8].weight, amount: tirePatchAmount },
       roverKit:
-      { type: data.supplyList[9].type, weight: data.supplyList[9].weight, amount: roverMainAmount },
+      { type: supplyList[9].type, weight: supplyList[9].weight, amount: roverMainAmount },
     };
     return supplyObj;
   };
