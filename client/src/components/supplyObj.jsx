4aef5ccbe7755e1d04be14ce091037ab712c3f96
Krista Singleton <kris.singlet231@gmail.com>
2020-12-09 23:57:02 -0700

    ...and an amountList for the final supply amounts

--- client/src/components/supplyObj.jsx
+++ client/src/components/supplyObj.jsx
@@ -2,28 +2,28 @@ import React from 'react';
 
 const SupplyObj = () => {
   console.log('supplyObj fired');
-  const getFinalSupplyObj = (supplyList) => {
+  const getFinalSupplyObj = (supplyList, amountList) => {
     const supplyObj = {
       oxygen:
-      { type: supplyList[0].type, weight: supplyList[0].weight, amount: oxyAmount },
+      { type: supplyList[0].type, weight: supplyList[0].weight, amount: amountList },
       food:
-      { type: supplyList[1].type, weight: supplyList[1].weight, amount: foodAmount },
+      { type: supplyList[1].type, weight: supplyList[1].weight, amount: amountList },
       water:
-      { type: supplyList[2].type, weight: supplyList[2].weight, amount: waterAmount },
+      { type: supplyList[2].type, weight: supplyList[2].weight, amount: amountList },
       clothes:
-      { type: supplyList[3].type, weight: supplyList[3].weight, amount: clothesAmount },
+      { type: supplyList[3].type, weight: supplyList[3].weight, amount: amountList },
       clothes2:
-      { type: supplyList[4].type, weight: supplyList[4].weight, amount: clothesAmount2 },
+      { type: supplyList[4].type, weight: supplyList[4].weight, amount: amountList },
       spaceSuit:
-      { type: supplyList[5].type, weight: supplyList[5].weight, amount: suitAmount },
+      { type: supplyList[5].type, weight: supplyList[5].weight, amount: amountList },
       spaceSuit2:
-      { type: supplyList[6].type, weight: supplyList[6].weight, amount: suitAmount2 },
+      { type: supplyList[6].type, weight: supplyList[6].weight, amount: amountList },
       aiKit:
-      { type: supplyList[7].type, weight: supplyList[7].weight, amount: AImainAmount },
+      { type: supplyList[7].type, weight: supplyList[7].weight, amount: amountList },
       tirePatch:
-      { type: supplyList[8].type, weight: supplyList[8].weight, amount: tirePatchAmount },
+      { type: supplyList[8].type, weight: supplyList[8].weight, amount: amountList },
       roverKit:
-      { type: supplyList[9].type, weight: supplyList[9].weight, amount: roverMainAmount },
+      { type: supplyList[9].type, weight: supplyList[9].weight, amount: amountList },
     };
     return supplyObj;
   };
