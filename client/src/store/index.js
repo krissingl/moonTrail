66ef3751d8c30079d4672f5702809144ebba45b6
Krista Singleton <kris.singlet231@gmail.com>
2020-12-19 19:48:57 -0700

    Fixed page turn reducer

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -3,3 +3,12 @@ import { createStore } from 'redux';
 const initialState = {
   page: 'main',
 };
+
+const reducer = (state = initialState, action) => {
+  if (action.type === 'changePage') {
+    return Object.assign({}, state, {
+      page: action.payload
+    });
+  }
+  return state;
+};
