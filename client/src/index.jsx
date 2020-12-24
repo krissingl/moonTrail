24c4466389cdca9632622434b2a12a072035d557
Krista Singleton <kris.singlet231@gmail.com>
2020-12-23 22:45:19 -0700

    Added Provider from react-redux to index.jsx file for store import

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,4 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app.jsx';
 
+import { Provider } from 'react-redux';
+
 ReactDOM.render(<App />, document.getElementById('app'));
