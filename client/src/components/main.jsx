3c4f54ecf1d4dee26ef9fc079933d9b5611769bb
Krista Singleton <kris.singlet231@gmail.com>
2020-12-24 22:27:44 -0700

    Successfully created an alter global redux state test button on main.jsx

--- client/src/components/main.jsx
+++ client/src/components/main.jsx
@@ -2,13 +2,23 @@ import React from 'react';
 import { connect } from 'react-redux';
 import classes from '../css/styles.css';
 
-const Main = ({ changePage, test }) => (
-  <div className={classes.noticePage}>
-    <h1>WELCOME_TO_MOON_TRAIL</h1>
-    <h3>{test}</h3>
-    <button type="button" className={classes.startBtn} onClick={() => { changePage('intro'); }}>START</button>
-  </div>
-);
+const Main = ({ changePage, test, dispatch }) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch({
+      type: 'test',
+      payload: 'ALtered Global Redux state TEST',
+    });
+  };
+  return (
+    <div className={classes.noticePage}>
+      <h1>WELCOME_TO_MOON_TRAIL</h1>
+      <h3>{test}</h3>
+      <button type="button" onClick={handleSubmit}>Change Test Message</button>
+      <button type="button" className={classes.startBtn} onClick={() => { changePage('intro'); }}>START</button>
+    </div>
+  );
+};
 
 const mapStateToProps = (state) => ({ test: state.test });
 const mapDispatchToProps = (dispatch) => ({
