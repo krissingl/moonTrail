fe24a99867debd3fffd12baab7f133f31f30f956
Krista Singleton <kris.singlet231@gmail.com>
2020-12-29 00:00:47 -0700

    Added changeCrew conditional to reducer func in redux store

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -11,12 +11,14 @@ const initialState = {
 const reducer = (state = initialState, action) => {
   if (action.type === 'test') {
     return { ...state, test: action.payload };
+  } if (action.type === 'changeCrew') {
+    return { ...state, crew: action.payload };
+  } if (action.type === 'changeRover') {
+    return { ...state, rover: action.payload };
   } if (action.type === 'supplyObjChange') {
     return { ...state, supplyObj: action.payload };
   } if (action.type === 'landmarkDistanceChange') {
     return { ...state, savedDistance: action.payload };
-  } if (action.type === 'changeRover') {
-    return { ...state, rover: action.payload };
   }
   return state;
 };
